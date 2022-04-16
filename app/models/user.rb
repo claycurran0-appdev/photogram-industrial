1afb2c5e44009d78ce7a03db08537a3a03cc5297
claycurran0 <ccurran@chicagobooth.edu>
2022-04-16 22:57:02 +0000

    Added follow_requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
 
   has_many :comments, foreign_key: "author_id"
+
+  has_many :followers, class_name: "User", foreign_key: "recipient_id"
+  has_many :leaders, class_name: "User", foreign_key: "sender_id"
   
 end
