36d2928cdc9024fc1df27006be87a7f9170021ec
claycurran0 <ccurran@chicagobooth.edu>
2022-04-17 16:22:00 +0000

    Added validations

--- db/migrate/20220417160816_add_default_to_private.rb
+++ db/migrate/20220417160816_add_default_to_private.rb
@@ -1,5 +1,5 @@
 class AddDefaultToPrivate < ActiveRecord::Migration[6.1]
   def change
-      change_column_default(:users, :private, true)
+    change_column_default(:users, :private, true)
   end
 end
